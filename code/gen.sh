d979e59b1e8f86f2545125147dc188b58d56e5b1
José Pedro Aguiar <josepedroaguiar@Air-de-Jose.lan>
2022-11-01 18:07:52 +0000

    Adicionar paragrafo  na section da cultura ao relatorio

--- code/gen.sh
+++ code/gen.sh
@@ -1,13 +1,21 @@
 #!/usr/bin/bash
-for (( j=1; j<=10; j++ ))
+#criar lista de nomes dos ficheiros testes
+files=""
+echo > fileName.txt
+for (( j=1; j<=5; j++ ))
 do
 maxflow=$((9999*$j))
-for (( j=1; j<=10; j++ ))
+
+for (( k=1; k<=5; k++ ))
 do
-maxcapacaty=$((9999*$j))
-  for (( c=1; c<=10; c++ ))
+maxcapacaty=$((9999*$k))
+cd inputs
+mkdir -p ./flow_$maxflow/capacaty_$maxcapacaty
+cd ..
+echo "Gerar inputs$maxcapacaty:"
+  for (( c=1; c<= 10; c++ ))
       do 
-      vert=$((2500*$c ))
+      vert=$((100*$c ))
 
       echo " $vert $per"
       #vert=$(($RANDOM%5555 ))
@@ -15,25 +23,22 @@ maxcapacaty=$((9999*$j))
       #p=$(($RANDOM%9999))
       #c=$(($RANDOM%99999))
       #per= "val=$(($RANDOM%101));val=/100.1" | bc 
-      echo "gerar input$c:"
+      
+      echo "  -> Gerar inputs$vert:"
       for (( i=1; i<=10; i++ ))
         do
-
         per=`echo "scale=2;var=$((($i*10)%101));var/=100;var" | bc`
-        echo "  gerar input$c$per"
-        python3 gen.py  $vert $per  $maxcapacaty $maxflow ./inputs/input$maxflow$maxcapacaty$c$per.txt
+        echo "    - Gerar input$c$per"
+        python3 gen.py  $vert $per  $maxcapacaty $maxflow ./inputs/flow_$maxflow/capacaty_$maxcapacaty/$c$per.txt
         done
     done
+    #criar lista de nomes dos ficheiros testes
+    search_dir=./inputs/flow_$maxflow/capacaty_$maxcapacaty/$c$per.txt
+    for entry in "$search_dir"/*.txt
+      do
+          files="$files$entry "
+      done
+    
   done
-done
-
-
-#criar lista de nomes dos ficheiros testes
-search_dir=./inputs
-files=""
-echo > fileName.txt
-for entry in "$search_dir"/*.txt
-do
-    files="$files$entry "
 done
 echo "$files " >> fileName.txt
--- code/run.sh
+++ code/run.sh
@@ -1,10 +1,17 @@
+search_dir=./inputs/flow_9999/capacaty_9999
+files=""
+echo > fileName.txt
+for entry in "$search_dir"/*.txt
+do
+    files="$files$entry "
+done
+echo "$files " >> fileName.txt
 
-files="input01.txt input02.txt"
 
 for  file in $files
     do
         echo "correr $file"
-        ./Dinic 9999 $file
-        ./EK 9999  $file
-        ./MPM 9999 $file
+        ./Dinic 9999 $file >> Dinic_1.txt
+        ./EK 9999  $file >> EK_1.txt
+        ./MPM 9999 $file  >> MPM_1.txt
 done
